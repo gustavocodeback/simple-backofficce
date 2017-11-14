41af2a3a61ac8e00984a910ae89637ce006b8cc9
gustavo villas boas <gu.boas13@gmail.com>
2017-11-14 10:15:16 -0200

    Remove os dados do mailgun

--- application/config/email.php
+++ application/config/email.php
@@ -5,15 +5,15 @@
  */
 
 // conexao
-$config['host'] = 'ssl://smtp.mailgun.org';
+$config['host'] = '';
 $config['port'] = 465;
 
 // apresentacao
-$config['from']      = 'postmaster@sandbox1fe1cc36a08141f28e9102e42b635f3a.mailgun.org';
-$config['from_name'] = 'Singular';
+$config['from']      = '';
+$config['from_name'] = '';
 
 // autenticacao
-$config['username'] = 'postmaster@sandbox1fe1cc36a08141f28e9102e42b635f3a.mailgun.org';
-$config['password'] = 'cc6f4f7895c44eab96ce5fb421080b18';
+$config['username'] = '';
+$config['password'] = '';
 
 /* end of file */
