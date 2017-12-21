d2359af6391316d2b18c7fafd2dfe60d4249b68e
gustavo villas boas <gu.boas13@gmail.com>
2017-12-21 08:27:57 -0200

    Muda o config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8000';
+$config['base_url'] = 'http://simplenews.tempsite.ws/';
 
 /*
 |--------------------------------------------------------------------------
@@ -543,7 +543,7 @@ $config['enable_cli'] = true;
 |
 |
 */
-$config['site_name'] = 'PHPMaker';
+$config['site_name'] = 'Simple';
 
 /*
 |--------------------------------------------------------------------------
