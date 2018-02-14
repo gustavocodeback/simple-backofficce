8b53edff84f8c755466a8b455b36ddb55f89dac6
Agatha Magalhaes <agathaemm@gmail.com>
2018-02-14 09:06:32 -0200

    Mostra a categoria geral na tela de gateways

--- frontend/pages/gateway/section.blade.php
+++ frontend/pages/gateway/section.blade.php
@@ -15,6 +15,15 @@
             </div>
         </div>
         @endforeach
+        <div class="col-sm-4 p-2">
+            <div {!! clickOpen( 'gateway/list/1' ) !!} class="clicable card bg-dark text-white p-0 z-depth-3">
+                <img class="card-img" src="{{ base_url( 'public/images/categoria_geral.png' ) }}" alt="Card image">
+                <div class="card-img-overlay p-0">
+                    <h5 class="card-title bg-success position-absolute p-2 z-depth-3"
+                        style="bottom: 1px">Categoria geral</h5>
+                </div>
+            </div>
+        </div>
 
     </div>
 @endsection
