843c09d7fcd49bfece228dc5293a7615ac1a06e2
Agatha Magalhaes <agathaemm@gmail.com>
2018-03-28 10:34:38 -0300

    Adiciona o controller de lojas

---
+++ application/controllers/web/Lojas.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Lojas extends SG_Controller {
+
+    /**
+     * __construct
+     * 
+     * método construtor
+     * 
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    /**
+     * index
+     * 
+     * método inicial
+     *
+     * @return void
+     */
+    public function index() {
+
+        //Detect special conditions devices
+        $iPod    = stripos($_SERVER['HTTP_USER_AGENT'],"iPod");
+        $iPhone  = stripos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+        $iPad    = stripos($_SERVER['HTTP_USER_AGENT'],"iPad");
+        $Android = stripos($_SERVER['HTTP_USER_AGENT'],"Android");
+        $webOS   = stripos($_SERVER['HTTP_USER_AGENT'],"webOS");
+
+        //do something with this information
+        if( $iPod || $iPhone ){
+            redirect( 'http://getsimple.com.br/www/' );
+            exit();
+        }else if($iPad){
+            redirect( 'http://getsimple.com.br/www/' );
+            exit();
+        }else if($Android){
+            redirect( 'https://play.google.com/store/apps/details?id=br.com.codeback.simple' );
+            exit();
+        }else if($webOS){
+            redirect( 'https://getsimple.com.br/' );
+            exit();
+		}
+    }
+}
+
+// End of file
